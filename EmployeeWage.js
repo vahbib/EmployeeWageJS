b400ac2b578c4cd065159512b317a4c3024e9534
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-09 10:21:20 +0530

    Calculate Daily Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,14 @@
 console.log("Welcome to Employee Wage Program");
+// UC1
+const IS_ABSENT = 0;
+let random = Math.floor(Math.random()* 10) % 2;
+if(random == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}
+else {
+    console.log("Employee is Present");
+}
 //UC 2
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
