673af8999e5dc50ce6aa9db557a9fbd503453413
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-09 10:39:16 +0530

    Calculate Employee Wage by Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,59 @@
 console.log("Welcome to Employee Wage Program");
+// UC1
+const IS_ABSENT = 0;
+let random = Math.floor(Math.random()* 10) % 2;
+if(random == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}
+else {
+    console.log("Employee is Present");
+}
+//UC 2
+// const IS_PART_TIME = 1;
+// const IS_FULL_TIME = 2;
+// const PART_TIME_HOURS = 4;
+// const FULL_TIME_HOURS = 8;
+// const WAGE_PER_HOUR = 20;
+
+// let empHrs = 0;
+// let options = Math.floor(Math.random() * 10) % 3;
+
+// switch (options) {
+//     case IS_FULL_TIME:
+//         empHrs = FULL_TIME_HOURS;
+//         break;
+//     case IS_PART_TIME:
+//         empHrs = PART_TIME_HOURS;
+//         break;
+//     default:
+//         empHrs = 0;
+// } 
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Employee wage is ", empWage);
+
+// UC 3
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let options = Math.floor(Math.random() * 10) % 3;
+demo(options);
+
+function demo(options) {
+    switch (options) {
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HOURS;
+            break;
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HOURS;
+            break;
+        default:
+            empHrs = 0;
+    } 
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Employee wage is ", empWage);
+}
