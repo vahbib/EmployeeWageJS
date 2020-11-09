1f7685da69e8c944b8bed8fe47861f532ceacb9b
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-09 10:08:58 +0530

    check employee is present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,11 @@
 console.log("Welcome to Employee Wage Program");
+// UC1
+const IS_ABSENT = 0;
+let random = Math.floor(Math.random()* 10) % 2;
+if(random == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}
+else {
+    console.log("Employee is Present");
+}
