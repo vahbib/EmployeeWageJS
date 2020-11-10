c9d14d5e191b45e78b84efe8bb56c168e01ae6e0
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-10 09:11:17 +0530

    Storing Employee Wage into an Array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,6 @@
 console.log("Welcome to Employee Wage Program");
+
+/* 
 // UC1
 // const IS_ABSENT = 0;
 // let random = Math.floor(Math.random()* 10) % 2;
@@ -71,7 +73,7 @@ function demo(options){
     }
 }
 let empHrs = 0;
-let WORKING_DAYS = 20;
+// let WORKING_DAYS = 20;
 
 // for(let day = 0; day < WORKING_DAYS ; day ++){
 //     let options = Math.floor(Math.random() * 10) % 3;
@@ -81,17 +83,67 @@ let WORKING_DAYS = 20;
 // }
 
 //UC5
-const MAX_HOURS_IN_MONTH = 100;
-let Total_EMP_HOURS = 0;
-let TOTAL_EMP_WAGE = 0;
-let TOTAL_WORKING_DAYS = 0;
-while (Total_EMP_HOURS < MAX_HOURS_IN_MONTH && TOTAL_WORKING_DAYS < WORKING_DAYS ) {
-    TOTAL_WORKING_DAYS++;
-    let options = Math.floor(Math.random() * 10) % 3;
-    empHrs = demo(options);
-    Total_EMP_HOURS += empHrs;
-    let empWage = empHrs * WAGE_PER_HOUR;
-        console.log("Day : "+TOTAL_WORKING_DAYS+ " | Total Hours "+Total_EMP_HOURS +" | Employee wage is:",empWage);
+// const MAX_HOURS_IN_MONTH = 100;
+// let Total_EMP_HOURS = 0;
+// let TOTAL_EMP_WAGE = 0;
+// let TOTAL_WORKING_DAYS = 0;
+// while (Total_EMP_HOURS < MAX_HOURS_IN_MONTH && TOTAL_WORKING_DAYS < WORKING_DAYS ) {
+//     TOTAL_WORKING_DAYS++;
+//     let options = Math.floor(Math.random() * 10) % 3;
+//     empHrs = demo(options);
+//     Total_EMP_HOURS += empHrs;
+//     let empWage = empHrs * WAGE_PER_HOUR;
+//         console.log("Day : "+TOTAL_WORKING_DAYS+ " | Total Hours "+Total_EMP_HOURS +" | Employee wage is:",empWage);
+// }
+// let totalEmpWage = Total_EMP_HOURS * WAGE_PER_HOUR;
+// console.log("Total Employee Wage: ",totalEmpWage);
+
+*/
+
+//UC 6
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+function demo(options){
+    switch(options){
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+     
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-let totalEmpWage = Total_EMP_HOURS * WAGE_PER_HOUR;
-console.log("Total Employee Wage: ",totalEmpWage);
+let empHrs = 0;
+
+function calcDailyWage(empHrs) 
+{
+    return empHrs * WAGE_PER_HOUR;
+}
+
+    const MAX_HRS_IN_MONTH = 100;
+    
+    const NUM_OF_WORKING_DAYS = 20;
+    
+    let totalEmpHrs = 0;
+    
+    let totalWorkingDays = 0;
+    
+    let empDailyWageArr = new Array();
+    
+    while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
+    {
+         totalWorkingDays++; 
+         let empCheck = Math.floor(Math.random() * 10) % 3; 
+         let empHrs = demo(empCheck); 
+         totalEmpHrs += empHrs; 
+    }
+    
+    empDailyWageArr.push(calcDailyWage(empHrs));
+    
+    let empWage = calcDailyWage(totalEmpHrs); 
+    console.log("Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
