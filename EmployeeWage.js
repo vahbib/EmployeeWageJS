8cba8ec8b23978d416756ef60ebceb6aec134af2
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-13 22:08:26 +0530

    Setting value in Map

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,8 +6,13 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let employeeDailyHrs = new Map();
-let employeeDailyWage = new Map();
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+let employeeDailyHrsMap = new Map();
+let employeeDailyWageMap = new Map();
 
 function demo(options){
     switch(options){
@@ -26,19 +31,18 @@ function calcDailyWage(empHrs)
 {
     return empHrs * WAGE_PER_HOUR;
 }
-
-    const MAX_HRS_IN_MONTH = 100;
-    const NUM_OF_WORKING_DAYS = 20;
-    let totalEmpHrs = 0;
-    let totalWorkingDays = 0;
-    let empDailyWageArr = new Array();
     
-    while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
-    {
-         totalWorkingDays++; 
-         let empCheck = Math.floor(Math.random() * 10) % 3; 
-         let empHrs = demo(empCheck); 
-         empDailyWageArr.push(calcDailyWage(empHrs));
-         totalEmpHrs += empHrs; 
-    }
-    console.log("Daily Wage Array: "+empDailyWageArr);
+while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
+{
+     totalWorkingDays++; 
+     let empCheck = Math.floor(Math.random() * 10) % 3; 
+     let empHrs = demo(empCheck); 
+     empDailyWageArr.push(calcDailyWage(empHrs));
+     totalEmpHrs += empHrs;
+
+     employeeDailyHrsMap.set(totalWorkingDays,empHrs);
+     employeeDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+}
+console.log("Daily Wage Array: "+empDailyWageArr);
+console.log(employeeDailyWageMap);
+console.log(employeeDailyHrsMap);
