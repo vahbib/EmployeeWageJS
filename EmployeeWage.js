4e7cd0a5f5fd24d28cfd4bd51cc78be874b922e0
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-13 19:28:49 +0530

    Storing daily wage in an array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,105 +1,4 @@
 console.log("Welcome to Employee Wage Program");
-
-/* 
-// UC1
-// const IS_ABSENT = 0;
-// let random = Math.floor(Math.random()* 10) % 2;
-// if(random == IS_ABSENT) {
-//     console.log("Employee is Absent");
-//     return;
-// }
-// else {
-//     console.log("Employee is Present");
-// }
-//UC 2
-// const IS_PART_TIME = 1;
-// const IS_FULL_TIME = 2;
-// const PART_TIME_HOURS = 4;
-// const FULL_TIME_HOURS = 8;
-// const WAGE_PER_HOUR = 20;
-
-// let empHrs = 0;
-// let options = Math.floor(Math.random() * 10) % 3;
-
-// switch (options) {
-//     case IS_FULL_TIME:
-//         empHrs = FULL_TIME_HOURS;
-//         break;
-//     case IS_PART_TIME:
-//         empHrs = PART_TIME_HOURS;
-//         break;
-//     default:
-//         empHrs = 0;
-// } 
-// let empWage = empHrs * WAGE_PER_HOUR;
-// console.log("Employee wage is ", empWage);
-
-// UC 3
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-
-// let empHrs = 0;
-// let options = Math.floor(Math.random() * 10) % 3;
-// demo(options);
-
-// function demo(options) {
-//     switch (options) {
-//         case IS_FULL_TIME:
-//             empHrs = FULL_TIME_HOURS;
-//             break;
-//         case IS_PART_TIME:
-//             empHrs = PART_TIME_HOURS;
-//             break;
-//         default:
-//             empHrs = 0;
-//     } 
-//     let empWage = empHrs * WAGE_PER_HOUR;
-//     console.log("Employee wage is ", empWage);
-// }
-// UC4
-
-function demo(options){
-    switch(options){
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-     
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-        default:
-            return 0;
-    }
-}
-let empHrs = 0;
-// let WORKING_DAYS = 20;
-
-// for(let day = 0; day < WORKING_DAYS ; day ++){
-//     let options = Math.floor(Math.random() * 10) % 3;
-//     empHrs = demo(options);
-//     let empWage = empHrs * WAGE_PER_HOUR;
-//     console.log("Day : "+day+" Employee wage is",empWage);
-// }
-
-//UC5
-// const MAX_HOURS_IN_MONTH = 100;
-// let Total_EMP_HOURS = 0;
-// let TOTAL_EMP_WAGE = 0;
-// let TOTAL_WORKING_DAYS = 0;
-// while (Total_EMP_HOURS < MAX_HOURS_IN_MONTH && TOTAL_WORKING_DAYS < WORKING_DAYS ) {
-//     TOTAL_WORKING_DAYS++;
-//     let options = Math.floor(Math.random() * 10) % 3;
-//     empHrs = demo(options);
-//     Total_EMP_HOURS += empHrs;
-//     let empWage = empHrs * WAGE_PER_HOUR;
-//         console.log("Day : "+TOTAL_WORKING_DAYS+ " | Total Hours "+Total_EMP_HOURS +" | Employee wage is:",empWage);
-// }
-// let totalEmpWage = Total_EMP_HOURS * WAGE_PER_HOUR;
-// console.log("Total Employee Wage: ",totalEmpWage);
-
-*/
-
 //UC 6
 
 const IS_PART_TIME = 1;
@@ -107,6 +6,9 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+let employeeDailyHrs = new Map();
+let employeeDailyWage = new Map();
+
 function demo(options){
     switch(options){
         case IS_FULL_TIME:
@@ -126,13 +28,9 @@ function calcDailyWage(empHrs)
 }
 
     const MAX_HRS_IN_MONTH = 100;
-    
     const NUM_OF_WORKING_DAYS = 20;
-    
     let totalEmpHrs = 0;
-    
     let totalWorkingDays = 0;
-    
     let empDailyWageArr = new Array();
     
     while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
@@ -140,10 +38,7 @@ function calcDailyWage(empHrs)
          totalWorkingDays++; 
          let empCheck = Math.floor(Math.random() * 10) % 3; 
          let empHrs = demo(empCheck); 
+         empDailyWageArr.push(calcDailyWage(empHrs));
          totalEmpHrs += empHrs; 
     }
-    
-    empDailyWageArr.push(calcDailyWage(empHrs));
-    
-    let empWage = calcDailyWage(totalEmpHrs); 
-    console.log("Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+    console.log("Daily Wage Array: "+empDailyWageArr);
